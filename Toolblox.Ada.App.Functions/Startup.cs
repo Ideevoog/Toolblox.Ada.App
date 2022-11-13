6398b8bd676a37a4f4c52aa6c81cacaeef54ee1b
sgraphics <silver.sepp@gmail.com>
2022-11-13 19:50:31 +0200

    fix singleton issue

---
+++ Toolblox.Ada.App.Functions/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Toolblox.Ada.App.Functions;
+using DinkToPdf;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+namespace Toolblox.Ada.App.Functions
+{
+	public class Startup : FunctionsStartup
+	{
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+			//builder.Services.AddHttpClient();
+
+			builder.Services.AddSingleton<SynchronizedConverter>((s)
+				=> new(new PdfTools()));
+
+			//builder.Services.AddSingleton<ILoggerProvider, MyLoggerProvider>();
+		}
+	}
+}
